4af5097114c14a731567fade2e8f28d71cfdb522
Eugene Syroezhkin <61776541+mega-yujin@users.noreply.github.com>
2022-07-14 09:57:38 +0300

    Deleted unused comments

--- calculator/tests.py
+++ calculator/tests.py
@@ -1,10 +1,3 @@
 from django.test import TestCase
-# from django_seed import Seed
-# from calculator.models import Recipe, Component
-#
-# seeder = Seed.seeder()
-# seeder.add_entity(Component, 20)
-# seeder.add_entity(Recipe, 25)
-# inserted_pks = seeder.execute()
 
 # Create your tests here.
